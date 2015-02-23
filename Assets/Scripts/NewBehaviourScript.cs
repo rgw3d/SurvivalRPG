7e35874f53b5c0f22c145387bef65cec5ca1546e
rgw3d <rgw3d@aol.com>
2015-02-23 10:52:39 -0500

    wow commit

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -13,6 +13,7 @@ public class NewBehaviourScript : MonoBehaviour {
 	public float movementDistance = .001f;
     public float upMovement = .001f;
     public float sideMovement = .05f;
+    public bool scalePlayer = false;
 
     public KeyCode upKey;
     public KeyCode downKey;
@@ -35,54 +36,57 @@ public class NewBehaviourScript : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 
-		if (Input.GetKey (upKey)) {
+        playerMovement();
+
+        if(scalePlayer)
+            playerScale();
+
+	}
+
+    private void playerScale() {
+        float distToBot = Mathf.Abs(yMin - transform.position.y);
+        Vector3 newScale = new Vector3(-.3f * distToBot + 2f, -.3f * distToBot + 2f, 1);
+        transform.localScale = newScale;
+    }
+
+    private void playerMovement() {
+        if (Input.GetKey(upKey)) {
             //transform.Translate(0, movementDistance, 0);
-			if(transform.position.y<yMax)
-           		rigidbody2D.AddForce(Vector2.up * upMovement);
-            if (currentSprite != backSprite){
+            if (transform.position.y < yMax)
+                rigidbody2D.AddForce(Vector2.up * upMovement);
+            if (currentSprite != backSprite) {
                 spriteRenderer.sprite = backSprite;
                 currentSprite = backSprite;
             }
-		}
-		if (Input.GetKey (downKey)) {
-			if(transform.position.y>yMin)
-            	rigidbody2D.AddForce(Vector2.up * -1 * upMovement);
-			//transform.Translate(0,-movementDistance,0);
-            if (currentSprite != frontSprite){
+        }
+        if (Input.GetKey(downKey)) {
+            if (transform.position.y > yMin)
+                rigidbody2D.AddForce(Vector2.up * -1 * upMovement);
+            //transform.Translate(0,-movementDistance,0);
+            if (currentSprite != frontSprite) {
                 spriteRenderer.sprite = frontSprite;
                 currentSprite = frontSprite;
             }
-		}
-		if (Input.GetKey(leftKey)) {
+        }
+        if (Input.GetKey(leftKey)) {
             rigidbody2D.AddForce(Vector2.right * -1 * sideMovement);
-			//transform.Translate(-movementDistance,0,0);
-            if (currentSprite != leftSprite){
+            //transform.Translate(-movementDistance,0,0);
+            if (currentSprite != leftSprite) {
                 spriteRenderer.sprite = leftSprite;
                 currentSprite = leftSprite;
             }
 
-		}
-		if (Input.GetKey (rightKey)) {
+        }
+        if (Input.GetKey(rightKey)) {
             rigidbody2D.AddForce(Vector2.right * sideMovement);
-			//transform.Translate(movementDistance,0,0);
-            if(currentSprite != rightSprite){
+            //transform.Translate(movementDistance,0,0);
+            if (currentSprite != rightSprite) {
                 spriteRenderer.sprite = rightSprite;
                 currentSprite = rightSprite;
             }
-		}
-
-        float distToBot = Mathf.Abs(yMin - transform.position.y);
-        Vector3 newScale = new Vector3(-.3f * distToBot + 2f,-.3f * distToBot + 2f,1);
-        transform.localScale = newScale;
-        //float distToBot = Mathf.Abs(yMin - transform.position.y);
-        //Vector3 newScale = new Vector3(-.5f * distToBot + .885f,-.125f * distToBot + .885f,1);
-        //transform.localScale = newScale;
+        }
         
-        ///if ymin = -1 is scale of 1, and 1 is scale of .75  (-1,1) and (1,.75)
-        ///-.125 x +.885 = y
-        // -
-
-	}
+    }
 }
