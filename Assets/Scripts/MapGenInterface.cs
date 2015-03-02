751a1d3e42835e6420f0048aa54bf12f21ffd043
rgw3d <rgw3d@aol.com>
2015-03-02 10:57:53 -0500

    updated so that it works

--- Assets/Scripts/MapGenInterface.cs
+++ Assets/Scripts/MapGenInterface.cs
@@ -2,34 +2,34 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public interface MapGenInterface : MonoBehaviour {
+public interface MapGenInterface {
 
     /*
      * This method will be used to return the list of all the centers of rooms 
      * All the centers of the rooms are nodes, and they are returned in a 
      * list of Vector2s, containing the x and y coordinate
      */
-    public List<Vector2> initRoomNodes();
+    List<Vector2> initRoomNodes();
 
     /*
      * This method will draw a room around the player. 
      * Player must start in room.
      * duh
      */ 
-    public Vector2 initBaseNode();
+    Vector2 initBaseNode();
 
 
     /*
      * This method will create the rooms and return them grouped together in a list stored inside a list
      * 
      */ 
-    public List<List<Vector2>> createRooms();
+    List<List<Vector2>> createRooms();
 
     /*
      * This will draw all the rooms based on the locations that it recieves
      * 
      */ 
-    public void drawRooms(List<List<Vector2>> roomsCoordinates);
+    void drawRooms(List<List<Vector2>> roomsCoordinates);
     
     }
 
