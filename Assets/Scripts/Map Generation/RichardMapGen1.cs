6b7499c62935a79d3d63587e652bdf0627fe1128
rgw3d <rgw3d@aol.com>
2015-03-03 10:58:55 -0500

    starting to mess with this script

--- Assets/Scripts/Map Generation/RichardMapGen1.cs
+++ Assets/Scripts/Map Generation/RichardMapGen1.cs
@@ -1,15 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class RichardMapGen1 : MonoBehaviour {
+public class RichardMapGen1 : MonoBehaviour, MapGenInterface {
 
-	// Use this for initialization
-	void Start () {
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
