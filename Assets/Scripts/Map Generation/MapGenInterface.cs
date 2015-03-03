0fbe234da42366de98a3c1f37f801b7036dfc721
Steven Leonis <Spwizzard2@gmail.com>
2015-03-03 11:03:00 -0500

    ok

--- Assets/Scripts/Map Generation/MapGenInterface.cs
+++ Assets/Scripts/Map Generation/MapGenInterface.cs
@@ -10,27 +10,27 @@ public interface MapGenInterface {
     * Player must start in room.
     * duh
     */
-    Vector2 initBaseNode();
+    Tile initBaseNode();
 
     /*
      * This method will be used to return the list of all the centers of rooms 
      * All the centers of the rooms are nodes, and they are returned in a 
      * list of Vector2s, containing the x and y coordinate
      */
-    List<Vector2> initRoomNodes();
+    List<Tile> initRoomNodes();
 
 
     /*
      * This method will create the rooms and return them grouped together in a list stored inside a list
      * 
      */ 
-    List<List<Vector2>> createRooms();
+    List<List<Tile>> createRooms();
 
     /*
      * This will draw all the rooms based on the locations that it recieves
      * 
      */ 
-    void drawRooms(List<List<Vector2>> roomsCoordinates);
+    void drawRooms(List<List<Tile>> roomsCoordinates);
 
 
     /*
