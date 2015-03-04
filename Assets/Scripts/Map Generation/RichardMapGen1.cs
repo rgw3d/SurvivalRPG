9f104043e650e3e166f3be8e796b899443c24da1
rgw3d <rgw3d@aol.com>
2015-03-04 10:35:13 -0500

    updating map gen script to correct method signatues

--- Assets/Scripts/Map Generation/RichardMapGen1.cs
+++ Assets/Scripts/Map Generation/RichardMapGen1.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class RichardMapGen1 : MonoBehaviour, MapGenInterface {
 
@@ -9,32 +10,32 @@ public class RichardMapGen1 : MonoBehaviour, MapGenInterface {
     }
 
 
-    /*
+     /*
     * This method will draw a room around the player. 
     * Player must start in room.
     * duh
     */
-    Vector2 initBaseNode();
+    List<Tile> initBaseNode(List<Tile> tileList);
 
     /*
      * This method will be used to return the list of all the centers of rooms 
      * All the centers of the rooms are nodes, and they are returned in a 
      * list of Vector2s, containing the x and y coordinate
      */
-    List<Vector2> initRoomNodes();
+    List<Tile> initRoomNodes();
 
 
     /*
      * This method will create the rooms and return them grouped together in a list stored inside a list
      * 
      */ 
-    List<List<Vector2>> createRooms();
+    List<List<Tile>> createRooms();
 
     /*
      * This will draw all the rooms based on the locations that it recieves
      * 
      */ 
-    void drawRooms(List<List<Vector2>> roomsCoordinates);
+    void drawRooms(List<List<Tile>> roomsCoordinates);
 
 
     /*
@@ -42,6 +43,8 @@ public class RichardMapGen1 : MonoBehaviour, MapGenInterface {
      * 
      */ 
     void fillBackground();
+    
+    
 
 
 	
