c24de22362be5d1131cbca738a6b0c4ff577f1b9
rgw3d <rgw3d@aol.com>
2015-03-04 10:43:15 -0500

    Making empty shells of methods

--- Assets/Scripts/Map Generation/RichardMapGen1.cs
+++ Assets/Scripts/Map Generation/RichardMapGen1.cs
@@ -15,34 +15,48 @@ public class RichardMapGen1 : MonoBehaviour, MapGenInterface {
     * Player must start in room.
     * duh
     */
-    List<Tile> initBaseNode(List<Tile> tileList);
+    List<Tile> initBaseNode(List<Tile> tileList) {
+        return new List<Tile>();
+    }
 
     /*
      * This method will be used to return the list of all the centers of rooms 
      * All the centers of the rooms are nodes, and they are returned in a 
      * list of Vector2s, containing the x and y coordinate
      */
-    List<Tile> initRoomNodes();
+    List<Tile> initRoomNodes() {
+
+        return new List<Tile>();
+    }
 
 
     /*
      * This method will create the rooms and return them grouped together in a list stored inside a list
      * 
-     */ 
-    List<List<Tile>> createRooms();
+     */
+    List<List<Tile>> createRooms() {
+        return new List<List<Tile>>();
+    }
+
 
     /*
      * This will draw all the rooms based on the locations that it recieves
      * 
-     */ 
-    void drawRooms(List<List<Tile>> roomsCoordinates);
+     */
+    void drawRooms(List<List<Tile>> roomsCoordinates) { 
+        
+
+    }
 
 
     /*
      * This should fill the background 
      * 
-     */ 
-    void fillBackground();
+     */
+    void fillBackground() { 
+    
+    }
+
     
     
 
