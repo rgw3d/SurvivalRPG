9f8a439123ae7b3f31d385dddc932dfdd4039457
rgw3d <rgw3d@aol.com>
2015-03-12 08:34:47 -0400

    Changed the gitignore really

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -50,7 +50,7 @@ public class PlayerControl : MonoBehaviour, PlayerClass {
 	// Update is called once per frame
 	void FixedUpdate () {
         if (Input.GetKey(KeyCode.E)) { //just a test of the ability to work
-            
+            DelegateHolder.TriggerPlayerStatChange(StatType.Score, 1f);   
         }
 
         playerMovement();
