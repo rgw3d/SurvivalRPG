c4c7c98add597b37f3ac7e71fccd3a558e98ae8d
rgw3d <rgw3d@aol.com>
2015-04-03 20:19:16 -0400

    More formatting.  Cleaning up variable names and such.  Also cleaned up CreateCharacter()

--- Assets/Scripts/Network/PhotonNetworkManager.cs
+++ Assets/Scripts/Network/PhotonNetworkManager.cs
@@ -4,26 +4,23 @@ using System;
 
 public class PhotonNetworkManager : MonoBehaviour {
 
-    private string roomName = "Example Room Name";
-    private string playerName = "Player Name";
-    private PlayerStats.CharacterClass playerClass = PlayerStats.CharacterClass.Fighter;
-    private float playerClassSlider = 1;
-    private float playerSelectSlider = 0;
-    private RoomInfo[] roomsList;
-    public static bool isHost = false;
-    private bool displayPopup = false;
+    private string _roomName = "Example Room Name";
+    private RoomInfo[] _roomsList;
+    public static bool IsHost = false;
+
+    private bool _displayBadNamePopup = false;
+    private float _playerClassSlider = 1;
+    private float _playerSelectSlider = 0;
+    private string _playerName = "Player Name";
+    private PlayerStats.CharacterClass _playerClass = PlayerStats.CharacterClass.Fighter;
 
     void Start() {
         PhotonNetwork.ConnectUsingSettings("0.1");
-        DelegateHolder.TriggerGenerateAndRenderMap();
-        //PlayerPrefs.DeleteAll();
     }
 
-    void OnGUI()
-    {
-        //NotConnectedToRoom();
-        if(displayPopup)
-            DisplayPopup();
+    void OnGUI() {
+        if(_displayBadNamePopup)
+            DisplayBadNamePopup();
         
         if (!PhotonNetwork.connected)
         {
@@ -45,16 +42,16 @@ public class PhotonNetworkManager : MonoBehaviour {
                 GUILayout.BeginVertical(GUILayout.MinWidth(Screen.width / 4));//Create Room
                     if (GUILayout.Button("Create Room")) {
                         RoomOptions roomOptions = new RoomOptions() { isVisible = true, maxPlayers = 4, isOpen = true };
-                        PhotonNetwork.CreateRoom(roomName, roomOptions, TypedLobby.Default);
+                        PhotonNetwork.CreateRoom(_roomName, roomOptions, TypedLobby.Default);
                     }
-                    roomName = GUILayout.TextField(roomName, 20);
+                    _roomName = GUILayout.TextField(_roomName, 20);
                 GUILayout.EndVertical();
 
                 GUILayout.BeginVertical(GUILayout.MinWidth(Screen.width / 4));//Join Room
-                    if (roomsList != null) {
-                        for (int i = 0; i < roomsList.Length; i++) {
-                            if (GUILayout.Button("Join " + roomsList[i].name))
-                                PhotonNetwork.JoinRoom(roomsList[i].name);
+                    if (_roomsList != null) {
+                        for (int i = 0; i < _roomsList.Length; i++) {
+                            if (GUILayout.Button("Join " + _roomsList[i].name))
+                                PhotonNetwork.JoinRoom(_roomsList[i].name);
                         }
                     }
                 GUILayout.EndVertical();
@@ -62,8 +59,8 @@ public class PhotonNetworkManager : MonoBehaviour {
                 GUILayout.BeginVertical(GUILayout.MinWidth(Screen.width / 4));//Select Player
                     GUILayout.Label("Selected Player");
                     string[] allplayerNames = PlayerPrefs.GetString(GameControl.PLAYERNAMESKEY).Split(',');
-                    playerSelectSlider = GUILayout.HorizontalSlider(playerSelectSlider, 0f, (float)allplayerNames.Length-1);
-                    int playerIndex = (int)Mathf.Round(playerSelectSlider);
+                    _playerSelectSlider = GUILayout.HorizontalSlider(_playerSelectSlider, 0f, (float)allplayerNames.Length-1);
+                    int playerIndex = (int)Mathf.Round(_playerSelectSlider);
                     GUILayout.Box("Name: " +allplayerNames[playerIndex] + "\n" 
                         + "Class: "+ PlayerPrefs.GetInt(GameControl.PLAYERCLASSKEY + allplayerNames[playerIndex]) +"\n"
                         + "Max Health: " + PlayerPrefs.GetInt(GameControl.PLAYERMAXHEALTH + allplayerNames[playerIndex]));
@@ -74,19 +71,19 @@ public class PhotonNetworkManager : MonoBehaviour {
                     if (GUILayout.Button("Create Character")) {
                         CreateCharacter();
                     }
-                    playerName = GUILayout.TextField(playerName, 12);
+                    _playerName = GUILayout.TextField(_playerName, 12);
                 
-                    GUILayout.Box("Class:" + playerClass);
-                    playerClassSlider = GUILayout.HorizontalSlider(playerClassSlider, 0f, 2f);
-                    switch (Mathf.FloorToInt(playerClassSlider)) {
+                    GUILayout.Box("Class: " + _playerClass);
+                    _playerClassSlider = GUILayout.HorizontalSlider(_playerClassSlider, 0f, 2f);
+                    switch ((int)Mathf.Round(_playerClassSlider)) {
                         case 0:
-                            playerClass = PlayerStats.CharacterClass.Fighter;
+                            _playerClass = PlayerStats.CharacterClass.Fighter;
                             break;
                         case 1:
-                            playerClass = PlayerStats.CharacterClass.Mage;
+                            _playerClass = PlayerStats.CharacterClass.Mage;
                             break;
                         case 2:
-                            playerClass = PlayerStats.CharacterClass.Healer;
+                            _playerClass = PlayerStats.CharacterClass.Healer;
                             break;
                     }
                 GUILayout.EndVertical();
@@ -98,51 +95,58 @@ public class PhotonNetworkManager : MonoBehaviour {
     }
 
     private void CreateCharacter() {
-        //what do
+        if (_playerName.Contains(",") || _playerName.Contains(" ")) {//If the name contains bad characters then it is rejected
+            StartCoroutine(TextPopup(.5f));
+            return;
+        }
         string[] allplayerNames = PlayerPrefs.GetString(GameControl.PLAYERNAMESKEY).Split(',');
         foreach (string s in allplayerNames) {
-            if (s.Equals(playerName)) {
+            if (s.Equals(_playerName)) {//if any of the same name exist then the name is rejected
                 StartCoroutine(TextPopup(.5f));
                 return;
             }
         }
-        if (playerName.Contains(",") || playerName.Contains(" ")) {
-
-            return;
-        }
+        
 
         if (PlayerPrefs.GetString(GameControl.PLAYERNAMESKEY).Equals("")) {
-            PlayerPrefs.SetString(GameControl.PLAYERNAMESKEY, playerName);
+            PlayerPrefs.SetString(GameControl.PLAYERNAMESKEY, _playerName);
         }
         else {
-            PlayerPrefs.SetString(GameControl.PLAYERNAMESKEY, PlayerPrefs.GetString(GameControl.PLAYERNAMESKEY) +","+ playerName);
+            PlayerPrefs.SetString(GameControl.PLAYERNAMESKEY, PlayerPrefs.GetString(GameControl.PLAYERNAMESKEY) +","+ _playerName);
         }
         
-        PlayerPrefs.SetInt(GameControl.PLAYERCLASSKEY + playerName, (int)playerClass);
+        PlayerPrefs.SetInt(GameControl.PLAYERCLASSKEY + _playerName, (int)_playerClass);
 
-        if (playerClass == PlayerStats.CharacterClass.Fighter) {
-            PlayerPrefs.SetInt(GameControl.PLAYERMAXHEALTH + playerName, 1000);
+        if (_playerClass == PlayerStats.CharacterClass.Fighter) {
+            PlayerPrefs.SetInt(GameControl.PLAYERMAXHEALTH + _playerName, 1000);
             //do something 
             //set base stats
         }
-        else if (playerClass == PlayerStats.CharacterClass.Healer) {
+        else if (_playerClass == PlayerStats.CharacterClass.Healer) {
             //do something 
         }
-        else if (playerClass == PlayerStats.CharacterClass.Mage) {
+        else if (_playerClass == PlayerStats.CharacterClass.Mage) {
             //do something 
         }
+    }
 
+    IEnumerator TextPopup(float waitTime) {
+        _displayBadNamePopup = true;
+        yield return new WaitForSeconds(waitTime);
+        _displayBadNamePopup = false;
 
-
+    }
+    void DisplayBadNamePopup() {
+        GUI.Box(new Rect(2 * Screen.width / 5, Screen.height / 2, Screen.width / 5, Screen.height / 10), "Bad Name");
     }
 
     void OnReceivedRoomListUpdate() {
-        roomsList = PhotonNetwork.GetRoomList();
+        _roomsList = PhotonNetwork.GetRoomList();
     }
 
     void OnCreatedRoom() {//if this user created the room, then this is called
         Debug.Log("Created Room. Connected to room");
-        isHost = true;
+        IsHost = true;
         DelegateHolder.TriggerGenerateAndRenderMap();
     }
 
@@ -153,15 +157,7 @@ public class PhotonNetworkManager : MonoBehaviour {
         Debug.Log("Joined Room");
     }
 
-    IEnumerator TextPopup(float waitTime) {
-        displayPopup = true;
-        yield return new WaitForSeconds(waitTime);
-        displayPopup = false;
-
-    }
-    void DisplayPopup(){
-        GUI.Box(new Rect(2 * Screen.width / 5, Screen.height / 2, Screen.width / 5, Screen.height / 10), "Bad Name");
-    }
+    
 
 
     
