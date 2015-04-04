9be1ba89a6ec8faee38d1e43dfdc44f58884034b
rgw3d <rgw3d@aol.com>
2015-04-03 20:09:36 -0400

    Removed old code (it was already commented out) and indented the code between GUILayout.BeginArea() and GUILayout.EndArea()

--- Assets/Scripts/Network/PhotonNetworkManager.cs
+++ Assets/Scripts/Network/PhotonNetworkManager.cs
@@ -39,89 +39,62 @@ public class PhotonNetworkManager : MonoBehaviour {
     private void NotConnectedToRoom() {
 
         GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
-        GUILayout.BeginHorizontal();
-        GUILayout.FlexibleSpace();
-
-            GUILayout.BeginVertical(GUILayout.MinWidth(Screen.width / 4));//Create Room
-                if (GUILayout.Button("Create Room")) {
-                    RoomOptions roomOptions = new RoomOptions() { isVisible = true, maxPlayers = 4, isOpen = true };
-                    PhotonNetwork.CreateRoom(roomName, roomOptions, TypedLobby.Default);
-                }
-                roomName = GUILayout.TextField(roomName, 20);
-            GUILayout.EndVertical();
-
-            GUILayout.BeginVertical(GUILayout.MinWidth(Screen.width / 4));//Join Room
-                if (roomsList != null) {
-                    for (int i = 0; i < roomsList.Length; i++) {
-                        if (GUILayout.Button("Join " + roomsList[i].name))
-                            PhotonNetwork.JoinRoom(roomsList[i].name);
-                    }
-                }
-            GUILayout.EndVertical();
-           
-            GUILayout.BeginVertical(GUILayout.MinWidth(Screen.width / 4));//Select Player
-                GUILayout.Label("Selected Player");
-                string[] allplayerNames = PlayerPrefs.GetString(GameControl.PLAYERNAMESKEY).Split(',');
-                playerSelectSlider = GUILayout.HorizontalSlider(playerSelectSlider, 0f, (float)allplayerNames.Length-1);
-                int playerIndex = (int)Mathf.Round(playerSelectSlider);
-                GUILayout.Box("Name: " +allplayerNames[playerIndex] + "\n" 
-                    + "Class: "+ PlayerPrefs.GetInt(GameControl.PLAYERCLASSKEY + allplayerNames[playerIndex]) +"\n"
-                    + "Max Health: " + PlayerPrefs.GetInt(GameControl.PLAYERMAXHEALTH + allplayerNames[playerIndex]));
-                    
-            GUILayout.EndVertical();
-
-            GUILayout.BeginVertical(GUILayout.MinWidth(Screen.width / 4));//Create Player
-                if (GUILayout.Button("Create Character")) {
-                    CreateCharacter();
-                }
-                playerName = GUILayout.TextField(playerName, 12);
-                
-                GUILayout.Box("Class:" + playerClass);
-                playerClassSlider = GUILayout.HorizontalSlider(playerClassSlider, 0f, 2f);
-                switch (Mathf.FloorToInt(playerClassSlider)) {
-                    case 0:
-                        playerClass = PlayerStats.CharacterClass.Fighter;
-                        break;
-                    case 1:
-                        playerClass = PlayerStats.CharacterClass.Mage;
-                        break;
-                    case 2:
-                        playerClass = PlayerStats.CharacterClass.Healer;
-                        break;
-                }
-            GUILayout.EndVertical();
-
+            GUILayout.BeginHorizontal();
             GUILayout.FlexibleSpace();
-        GUILayout.EndHorizontal();
+
+                GUILayout.BeginVertical(GUILayout.MinWidth(Screen.width / 4));//Create Room
+                    if (GUILayout.Button("Create Room")) {
+                        RoomOptions roomOptions = new RoomOptions() { isVisible = true, maxPlayers = 4, isOpen = true };
+                        PhotonNetwork.CreateRoom(roomName, roomOptions, TypedLobby.Default);
+                    }
+                    roomName = GUILayout.TextField(roomName, 20);
+                GUILayout.EndVertical();
+
+                GUILayout.BeginVertical(GUILayout.MinWidth(Screen.width / 4));//Join Room
+                    if (roomsList != null) {
+                        for (int i = 0; i < roomsList.Length; i++) {
+                            if (GUILayout.Button("Join " + roomsList[i].name))
+                                PhotonNetwork.JoinRoom(roomsList[i].name);
+                        }
+                    }
+                GUILayout.EndVertical();
+           
+                GUILayout.BeginVertical(GUILayout.MinWidth(Screen.width / 4));//Select Player
+                    GUILayout.Label("Selected Player");
+                    string[] allplayerNames = PlayerPrefs.GetString(GameControl.PLAYERNAMESKEY).Split(',');
+                    playerSelectSlider = GUILayout.HorizontalSlider(playerSelectSlider, 0f, (float)allplayerNames.Length-1);
+                    int playerIndex = (int)Mathf.Round(playerSelectSlider);
+                    GUILayout.Box("Name: " +allplayerNames[playerIndex] + "\n" 
+                        + "Class: "+ PlayerPrefs.GetInt(GameControl.PLAYERCLASSKEY + allplayerNames[playerIndex]) +"\n"
+                        + "Max Health: " + PlayerPrefs.GetInt(GameControl.PLAYERMAXHEALTH + allplayerNames[playerIndex]));
+                    
+                GUILayout.EndVertical();
+
+                GUILayout.BeginVertical(GUILayout.MinWidth(Screen.width / 4));//Create Player
+                    if (GUILayout.Button("Create Character")) {
+                        CreateCharacter();
+                    }
+                    playerName = GUILayout.TextField(playerName, 12);
+                
+                    GUILayout.Box("Class:" + playerClass);
+                    playerClassSlider = GUILayout.HorizontalSlider(playerClassSlider, 0f, 2f);
+                    switch (Mathf.FloorToInt(playerClassSlider)) {
+                        case 0:
+                            playerClass = PlayerStats.CharacterClass.Fighter;
+                            break;
+                        case 1:
+                            playerClass = PlayerStats.CharacterClass.Mage;
+                            break;
+                        case 2:
+                            playerClass = PlayerStats.CharacterClass.Healer;
+                            break;
+                    }
+                GUILayout.EndVertical();
+
+                GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
         GUILayout.EndArea();
-
-
-        /*
-        // Create Room
-        if (GUI.Button(new Rect(Screen.width/6, Screen.height/6, Screen.width/6, Screen.height/6), "Create Room")) {
-            RoomOptions roomOptions = new RoomOptions() { isVisible = true, maxPlayers = 4, isOpen = true };
-            PhotonNetwork.CreateRoom(roomName, roomOptions, TypedLobby.Default);
-        }
-        roomName = GUI.TextField(new Rect(Screen.width / 6, 2 * Screen.height / 6, Screen.width / 6, Screen.height / 12), roomName, 20);
-
-        // Join Room
-        if (roomsList != null) {
-            for (int i = 1; i <= roomsList.Length; i++) {
-                if (GUI.Button(new Rect(Screen.width / 6,  12 * i* Screen.height / 24, Screen.width / 6, Screen.height / 12), "Join " + roomsList[i].name))
-                    PhotonNetwork.JoinRoom(roomsList[i].name);
-            }
-        }
-
-
-        //Create Character 
-        if (GUI.Button(new Rect(4 *Screen.width / 6, Screen.height / 6, Screen.width / 6, Screen.height / 6), "Create Player")) {
-            //create player
-        }
-        playerName = GUI.TextField(new Rect(4 * Screen.width / 6, 2 * Screen.height / 6, Screen.width / 6, Screen.height / 12), playerName, 12);
-        GUI.
-         * */
         
-
     }
 
     private void CreateCharacter() {
