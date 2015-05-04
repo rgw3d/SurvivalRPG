5cfcb6bbaffba597751418fc6e6f8ccb8b750d8d
rgw3d <rgw3d@aol.com>
2015-05-04 10:51:26 -0400

    Updated the code in the sword class, so that there will only be triggers of hits IF the sword on the same side as the player on the network

--- Assets/Scripts/Player/SwordHitbox/Sword.cs
+++ Assets/Scripts/Player/SwordHitbox/Sword.cs
@@ -6,15 +6,17 @@ public class Sword : MonoBehaviour {
 	public int swordDirection;
 	public bool isAttacking = false;
 
+
 	void Start(){
-		DelegateHolder.OnPlayerAttack += isPlayerAttacking;
+        if(GetComponentInParent<PhotonView>().isMine)
+            DelegateHolder.OnPlayerAttack += isPlayerAttacking;
+        else
+            collider2D.enabled = false;//should disable the colliders completly on this side
 	}
 
 	void OnTriggerStay2D(Collider2D other){
-        if(other.tag == "Enemy")
-        Debug.Log("Is Collission");
 		if(isAttacking){
-			if(other.tag == "Enemy"){
+			if(other.tag =="Enemy"){
 				Debug.Log("Attacked an enemy in direction " + swordDirection);
 			}
 		}
