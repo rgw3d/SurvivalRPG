cfc34ca183979609e78f6247838b3bc67cb2737e
rgw3d <rgw3d@aol.com>
2015-05-14 09:33:11 -0400

    Camera does not move, player points to the mouse

--- Assets/Scripts/Player/PlayerControl.cs
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -58,16 +58,17 @@ public class PlayerControl : Photon.MonoBehaviour{
         standing = 2,
     }
 
-	void Update(){
-		Vector2 positionOnScreen = transform.position;
-		Vector2 mouseOnScreen = Input.mousePosition;
-		float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
-		transform.rotation =  Quaternion.Euler (new Vector3(0f,0f,angle));
-	}
+    void Update() {
+        Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+        Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+        Camera.main.transform.rotation = Quaternion.EulerRotation(0, 0, 0);
+    }
 
-	float AngleBetweenTwoPoints(Vector3 a, Vector3 b) {
-		return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
-	}
+    float AngleBetweenTwoPoints(Vector3 a, Vector3 b) {
+        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+    }
 
 	// Update is called once per frame
 	void FixedUpdate () {
