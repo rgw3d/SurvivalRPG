d0d225cc2adfc9c8afcd45077116af8dc01a30df
Steven Leonis <Spwizzard2@gmail.com>
2015-05-20 12:57:59 -0400

    Fireball now works

--- Assets/Scripts/Abilities/Fireball.cs
+++ Assets/Scripts/Abilities/Fireball.cs
@@ -5,15 +5,23 @@ public class Fireball: Spell {
 
 	public int damage;
 	public float velocity;
+	public bool activated = false;
 
-
-	void FixedUpdate () {
-		//transform.Translate();
+	void Start(){
+		if(!GetComponentInParent<PhotonView>().isMine)
+			collider2D.enabled = false;//should disable the collider completly on this side
 	}
-
+ 
 	void OnTriggerStay2D(Collider2D other){
 		if(other.tag == "Enemy" || other.tag == "Wall"){
-			Destroy(gameObject);
+			activated = false;
+			rigidbody2D.velocity = Vector3.zero;
+			transform.position = new Vector2(-100,-100);
+			if(other.tag =="Enemy"){
+				EnemyBase enemyBase = other.GetComponent("EnemyBase") as EnemyBase;
+				enemyBase.OnEnemyAttacked(damage);
+			}
+
 		}
 	}
 }
