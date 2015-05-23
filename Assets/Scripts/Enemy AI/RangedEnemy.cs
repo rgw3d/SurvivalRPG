474d1fab1cec17bc93b875c158733df640f6d4ca
Steven Leonis <Spwizzard2@gmail.com>
2015-05-23 13:35:36 -0400

    I think i got tranquilizers working

--- Assets/Scripts/Enemy AI/RangedEnemy.cs
+++ Assets/Scripts/Enemy AI/RangedEnemy.cs
@@ -3,21 +3,32 @@ using System.Collections.Generic;
 
 public class RangedEnemy : EnemyBase {
 
+	private int _attackCooldown;
+
+	public RangedEnemyTranquilizer tranquilizerPrefab;
+	private GameObject _tranquilizer;
 
     public override void AttackBehavior() {
-        float angle = Mathf.Atan((transform.position.x - Target.transform.position.x) / (transform.position.y - Target.transform.position.y));
-        if (transform.position.y - Target.transform.position.y >= 0)
-            angle += Mathf.PI;
-        transform.Translate(Speed * Mathf.Sin(angle), Speed * Mathf.Cos(angle), 0);
+		if(_attackCooldown == 0){
+			_tranquilizer.transform.position = transform.position;
+			_tranquilizer.transform.rotation = transform.rotation;
+			_tranquilizer.rigidbody2D.velocity = Vector3.zero;
+			_tranquilizer.rigidbody2D.AddRelativeForce(tranquilizerPrefab.velocity * -1 * Vector2.right);
+			tranquilizerPrefab.activated = true;
+			_attackCooldown = tranquilizerPrefab.cooldown;
+		}
     }
 
+	public override void lowerCooldowns(){
+		if(_attackCooldown > 0){
+			_attackCooldown--;
+		}
+	}
 
-    void OnTriggerEnter2D(Collider2D collider) {
-        if (collider.tag == "Player")
-            Destroy(this);
-
-    }
-
+	public override void createNeededSubobjects(){
+		_tranquilizer = PhotonNetwork.Instantiate(tranquilizerPrefab.name, new Vector2(-100, -100), Quaternion.identity, 0);
+	}
+	
     void OnTriggerStay2D(Collider2D col) {
         if (PlayerList.Contains(col.gameObject)) {
             rigidbody2D.AddForce(col.gameObject.rigidbody2D.velocity * 10);
